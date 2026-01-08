93e47e3ca17c07187d4e81b62655c0863c2935c6
Rithanya Sekar <rithanyasekar918@gmail.com>
2026-01-08 11:34:58 -0800

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Navigation } from "./components/Navigation";
 import { Hero } from "./components/Hero";
-import { Education } from "./components/Education";
 import { Certifications } from "./components/Certifications";
 import { About } from "./components/About";
 import { Skills } from "./components/Skills";
@@ -23,7 +22,6 @@ export default function App() {
       
       <main>
         <Hero />
-        <Education />
         <Certifications />
         <About />
         <Skills />
