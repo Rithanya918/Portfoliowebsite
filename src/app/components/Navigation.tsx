fbab5a62e7f5f0732851e459079ffdd0c50f46f1
Rithanya Sekar <rithanyasekar918@gmail.com>
2026-01-13 14:38:37 -0800

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -9,7 +9,6 @@ export function Navigation() {
 
   const navItems = [
     { id: "hero", label: "Home" },
-    { id: "about", label: "About" },
     { id: "skills", label: "Skills" },
     { id: "projects", label: "Projects" },
     { id: "education", label: "Education" },
